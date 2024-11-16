0b97d13fed5325bec6d9f9b038bfbb440d8e0ca0
argietapic <tapicargie7@gmail.com>
2024-11-16 07:09:30 +0000

    feature/function-function guard added

--- functions.php
+++ functions.php
@@ -1,7 +1,23 @@
 <?php    
-    session_start(); // Start the session at the beginning
+    session_start();
     function connectDB() {
+        $servername = "localhost";
+        $email = "root";
+        $password = "";
+        $dbname = "dct-ccs-finals"; // Your database name
 
+        $conn = new mysqli($servername, $email, $password, $dbname);
+
+
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        return $conn;
     }    
+
+    function guard() {  
+   
+        }
+    }
 
 ?>
