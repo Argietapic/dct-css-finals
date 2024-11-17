ab7009e98609b2971743dc7d7072d347ae5f0c3f
argietapic <tapicargie7@gmail.com>
2024-11-17 06:06:56 +0000

    feature/register breadcrum

---
+++ admin/student/register.php
@@ -0,0 +1,21 @@
+<?php
+
+$Pagetitle = "Register Student";
+include("../partials/header.php");
+include("../partials/side-bar.php");
+?>
+
+<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 pt-5">    
+<div class="container">
+    <h2>Register a New Student</h2>
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="../dashboard.php">Dashboard</a></li>
+        </ol>
+    </nav>
+
+   
+        </div>
+   
+
+   
